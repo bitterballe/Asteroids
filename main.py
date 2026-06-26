2839625bfd5649aacb8520d6612e1b143dcb634b
andreas.nilsson <andreas.jan.nilsson@gmail.com>
2026-06-26 12:06:30 +0200

    Added groups for update

--- main.py
+++ main.py
@@ -11,8 +11,14 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0.0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2 , SCREEN_HEIGHT / 2)
 
+   
     while True:
         log_state()
 
@@ -20,9 +26,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
             
-        player.update(dt)    
+        updatable.update(dt)    
         screen.fill("black")
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
 
         # framerate 60 FPS
