6ea558881f2b78775e2d8d2a3ccb0be5db8b8110
andreas.nilsson <andreas.jan.nilsson@gmail.com>
2026-06-26 11:36:47 +0200

    Player move left/right

--- main.py
+++ main.py
@@ -15,12 +15,17 @@ def main():
 
     while True:
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        player.update(dt)    
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
+
+        # framerate 60 FPS
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
